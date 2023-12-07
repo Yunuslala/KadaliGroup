7f86e8431f88f034e5979e75a8c113579135b49c
Mohd Yunus <112767616+Yunuslala@users.noreply.github.com>
2023-12-07 18:08:40 +0530

    finalize task

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer';
+import "./Footer.css"
+
+
+function App() {
+
+  return (
+    <div className="App">
+    <div className='parnetContainer'>
+    <div className='ElementContainer'>
+     <div className='firstChild'>
+      <img src="https://kadali.my.canva.site/99wala/images/0c0266e15d76ba44735c33bcc0e27c74.svg" alt="logo" />
+      <div>
+        <p>99 wala</p>
+        <p>LEARN BEYOND BOUNDARIES</p>
+      </div>
+     </div>
+     <div className='BtnsDiv'>
+      <button>HOME</button>
+      <button>ABOUT US</button>
+      <button  style={{cursor:'pointer'}}>JOIN US</button>
+     </div>
+     <div className='TextBtnContainer'>
+      <p>Welcome..!</p>
+      <p>Navigate Knowledge, Reach</p>
+      <p>Success...!</p>
+      <button>Learn More</button>
+     </div>
+     <div className='ImgDiv'>
+     <div className='InnerImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/9ac4a8cfa8e842e424850744554ad1b5.svg" alt="backGround" />
+     </div>
+     <div className='UpperImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/c5728b2bab432a9fc87a96559e214050.png" alt="backGround" />
+     </div>
+     </div>
+
+
+     </div>
+     <div className='ElementContainer'>
+     <div className='firstChild'>
+      <img src="https://kadali.my.canva.site/99wala/images/0c0266e15d76ba44735c33bcc0e27c74.svg" alt="logo" />
+      <div>
+        <p>99 wala</p>
+        <p>LEARN BEYOND BOUNDARIES</p>
+      </div>
+     </div>
+     <div className='BtnsDiv'>
+      <button>HOME</button>
+      <button>ABOUT US</button>
+      <button  style={{cursor:'pointer'}}>JOIN US</button>
+     </div>
+     <div className='TextBtnContainer'>
+      <p>Explore Career..!</p>
+      <p>Explore Careers, Navigate</p>
+      <p>Success...!</p>
+      <button>Learn More</button>
+     </div>
+     <div className='ImgDiv'>
+     <div className='InnerImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/9ac4a8cfa8e842e424850744554ad1b5.svg" alt="backGround" />
+     </div>
+     <div className='UpperImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/7a7fba11b713ae9192ff562f34e54267.png" alt="backGround" />
+     </div>
+     </div>
+
+
+     </div>
+     
+     <div className='ElementContainer'>
+     <div className='firstChild'>
+      <img src="https://kadali.my.canva.site/99wala/images/0c0266e15d76ba44735c33bcc0e27c74.svg" alt="logo" />
+      <div>
+        <p>99 wala</p>
+        <p>LEARN BEYOND BOUNDARIES</p>
+      </div>
+     </div>
+     <div className='BtnsDiv'>
+      <button>HOME</button>
+      <button>ABOUT US</button>
+      <button  style={{cursor:'pointer'}}>JOIN US</button>
+     </div>
+     <div className='TextBtnContainer'>
+
+      <p>Resources</p>
+      <p>
+Resource Hub, Your Gateway to </p>
+      <p>     
+      Educational Excellence...!</p>
+      <button>Learn More</button>
+     </div>
+     <div className='ImgDiv'>
+     <div className='InnerImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/9ac4a8cfa8e842e424850744554ad1b5.svg" alt="backGround" />
+     </div>
+     <div className='UpperImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/62ca204fb576f4b4fafe03d53886bc5b.jpg" alt="backGround" />
+     </div>
+     </div>
+
+
+     </div>
+     <div className='ElementContainer'>
+     <div className='firstChild'>
+      <img src="https://kadali.my.canva.site/99wala/images/0c0266e15d76ba44735c33bcc0e27c74.svg" alt="logo" />
+      <div>
+        <p>99 wala</p>
+        <p>LEARN BEYOND BOUNDARIES</p>
+      </div>
+     </div>
+     <div className='BtnsDiv'>
+      <button>HOME</button>
+      <button>ABOUT US</button>
+      <button  style={{cursor:'pointer'}}>JOIN US</button>
+     </div>
+     <div className='TextBtnContainer'>
+      <p> ABOUT US</p>
+      <button>Learn More</button>
+     </div>
+     <div className='ImgDiv'>
+     <div className='InnerImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/9ac4a8cfa8e842e424850744554ad1b5.svg" alt="backGround" />
+     </div>
+     <div className='UpperImage'>
+     <img src="	https://kadali.my.canva.site/99wala/images/f884e6e0ce15d51b4eab66f6df7460fa.png" alt="backGround" />
+     </div>
+     </div>
+
+
+     </div>
+     <div className='ElementContainer'>
+     <div className='firstChild'>
+      <img src="https://kadali.my.canva.site/99wala/images/0c0266e15d76ba44735c33bcc0e27c74.svg" alt="logo" />
+      <div>
+        <p>99 wala</p>
+        <p>LEARN BEYOND BOUNDARIES</p>
+      </div>
+     </div>
+     <div className='BtnsDiv'>
+      <button>HOME</button>
+      <button>ABOUT US</button>
+      <button  style={{cursor:'pointer'}}>JOIN US</button>
+     </div>
+     <div className='TextBtnContainer'>
+      <p>JOIN US</p>
+
+      <button  style={{cursor:'pointer'}}>Contact Us</button>
+     </div>
+     <div className='ImgDiv'>
+     <div className='InnerImage'>
+     <img src="https://kadali.my.canva.site/99wala/images/9ac4a8cfa8e842e424850744554ad1b5.svg" alt="backGround" />
+     </div>
+     <div className='UpperImage'>
+     <img src="	https://kadali.my.canva.site/99wala/images/e36116d04a75cae42849f2ab1858a5fe.png" alt="backGround" />
+     </div>
+     </div>
+
+
+     </div>
+
+    </div>
+   <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <div className="firstChildFoot">
+          <p>Terms & Support</p>
+        </div>
+        <div className="footLastChild">
+          <p>Designed With Canvas</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
